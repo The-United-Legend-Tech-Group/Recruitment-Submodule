8beb399241b5c50f822e207712065a5f333e416d
Lakshy Rupani <lakshyrupani.lr@gmail.com>
2025-11-19 00:04:43 +0200

    app module

--- app.module.ts
+++ app.module.ts
@@ -2,13 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-
-
-
-
+import { RecruitmentModule } from './Recruitment/rec.module';
+import { OnBoardingModule } from './OnBoarding/OnBoarding.module';
+import { OffBoardingModule } from './OffBoarding/OffBoarding.module';
 
 @Module({
-  imports: [],
+  imports: [
+    RecruitmentModule,
+    OnBoardingModule,
+    OffBoardingModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
